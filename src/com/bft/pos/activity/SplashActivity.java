e443960ba6d63d7713011e0ca22668f9beca5a3f
yaozeyu <yzy000y@163.com>
2014-04-22 16:37:10 +0800

    小调整

--- src/com/bft/pos/activity/SplashActivity.java
+++ src/com/bft/pos/activity/SplashActivity.java
@@ -25,28 +25,8 @@ public class SplashActivity extends BaseActivity {
 				WindowManager.LayoutParams.FLAG_FULLSCREEN); // 设置全屏
 
 		setContentView(R.layout.splash_activity);
-//		getverifycode();
 		new SplashTask().execute();
 	}
-	
-	private void getverifycode(){
-		SimpleDateFormat sDateFormat = new SimpleDateFormat(
-				"yyyy-MM-dd hh:mm:ss");
-		String date = sDateFormat.format(new java.util.Date());
-		try {
-			Event event = new Event(null, "verifyCodes", null);
-			event.setTransfer("089021");
-			String fsk = "Get_ExtPsamNo|null";
-			event.setFsk(fsk);
-			HashMap<String, String> map = new HashMap<String, String>();
-			map.put("sendTime", date);
-			event.setStaticActivityDataMap(map);
-			event.trigger();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
 	class SplashTask extends AsyncTask<Object, Object, Object> {
 
 		@Override

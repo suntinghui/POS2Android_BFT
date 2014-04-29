352b5993d7c84af26cb79643f0f8f6617c34b513
Administrator <Administrator@PC36>
2014-04-29 18:51:56 +0800

    樊聪 设置支付密码成功

--- src/com/bft/pos/activity/FailActivity.java
+++ src/com/bft/pos/activity/FailActivity.java
@@ -1,14 +1,13 @@
 package com.bft.pos.activity;
 
-import com.bft.pos.R;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.WindowManager;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.bft.pos.R;
 
 /**
  * 失败
